d6762ddc8e9b9887e9acb60f734dc665b957fa9b
Alex Haščík <57464466+AlexHascik@users.noreply.github.com>
2023-06-09 23:29:03 +0200

    Update api.dart

--- lib/apis/api.dart
+++ lib/apis/api.dart
@@ -10,21 +10,21 @@ import '../data/user_details.dart';
 class API{
 
   //routes
-   String loginPath = "https://old.kolky.sk/user/login";
-   String institutionsList = "https://regapi.kolky.sk/institution/list";
-   String institutionDetail = "https://regapi.kolky.sk/institution/detail";
-   String registrationsList = "https://regapi.kolky.sk/registration/list";
-   String personLookup = "https://regapi.kolky.sk/person/list_v2";
-   String personDetail = "https://regapi.kolky.sk/person/detail";
-   String validateHash = "https://regapi.kolky.sk/user/validateHash";
-   String xAppAccesToken = "SK-81a92pceq-a123-9283-a5f7-0192qwp1mn44";
+   String loginPath = "hSfdsafdslogvvin";
+   String institutionsList = "Sfdsafdsist";
+   String institutionDetail = "htSfdsafdsetail";
+   String registrationsList = "hSfdsafdsist";
+   String personLookup = "httpsSfdsafds2";
+   String personDetail = "httpsSfdsafdsesil";
+   String validateHash = "hSfdsafdssh";
+   String xAppAccesToken = "Sfdsafds";
 
   
 
   //headers 
   Map<String, String> headers = {
     "Content-Type": 'application/json' , //application/json; charset=utf-8
-    "X-App-AccessToken": "SK-81a92pceq-a123-9283-a5f7-0192qwp1mn44"
+    "X-App-AccessToken": "Sfds"
   };
 
   Future<http.Response> sendRequest(String path, Map<String, String> header, Map<String, String> body) async{
